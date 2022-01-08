7ae66db4da196026225f724cbb01c02942f52783
ericgaspar <junk.eg@free.fr>
2022-01-08 09:06:04 +0100

    Fix

--- scripts/_common.sh
+++ scripts/_common.sh
@@ -5,7 +5,7 @@
 #=================================================
 
 # dependencies used by the app
-pkg_dependencies="postgresql postgresql-contrib python-virtualenv libffi-dev libssl-dev zlib1g-dev libncurses5-dev libncursesw5-dev libreadline-dev libffi6 sox libtag1v5 libmagic1 tzdata libmad0 libboost-all-dev libsndfile1 libid3tag0 ffmpeg redis-server"
+pkg_dependencies="postgresql postgresql-contrib python-virtualenv libffi-dev libssl-dev zlib1g-dev libncurses5-dev libncursesw5-dev libreadline-dev libffi6 sox libtag1v5 libmagic1 tzdata libmad0 libboost-all-dev libsndfile1 libid3tag0 ffmpeg"
 
 build_dependencies="libffi-dev cmake make pkg-config git libboost-all-dev libgd-dev libmad0-dev libsndfile-dev libid3tag0-dev libffi-dev"
 
